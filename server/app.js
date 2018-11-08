5335e66db87312467b70083c82b2dad3492b2244
jerrywu001 <57242263@163.com>
2018-11-08 11:49:24 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,9 +17,10 @@ app.use(cookieParser());
 app.use(session({
     secret: 'secret', // 对session id 相关的cookie 进行签名
     resave: true,
-    saveUninitialized: false, // 是否保存未初始化的会话
+    saveUninitialized: true, // 是否保存未初始化的会话
+    rolling: true,
     cookie: {
-        maxAge : 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+        expires : 1000 * 60 * 10, // 设置 session 的有效时间，单位毫秒，10分钟
     }
 }));
 
